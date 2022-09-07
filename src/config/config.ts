b25251b14022da176b2ba3373b7b48e625135f8f
AmiraMahana1996 <amiramahana96@gmail.com>
2022-09-08 00:49:16 +0200

    add database configration

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,10 +1,23 @@
-import {Client} from 'pg';
 
-const client = new Client({
-    host: "localhost",
-    port : 5432,
-    user: "postgres",
-    password: "4444love",
-    database: "migrationdb"
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const {
+    POSTGRES_HOST,
+    POSTGRES_DB,
+    POSTGRES_PASSWORD,
+    POSTGRES_USER,
+} = process.env
+
+let client = new Pool({})
+
+client = new Pool({
+    host: POSTGRES_HOST,
+    database: POSTGRES_DB,
+    user: POSTGRES_USER,
+    password: POSTGRES_PASSWORD,
+    port: 5432
 })
+
 export default client;
---
+++ src/database.ts
@@ -0,0 +1,34 @@
+// import dotenv from 'dotenv';
+// import { Pool } from 'pg';
+
+// dotenv.config();
+
+// let client = new Pool({})
+
+// const {
+//   POSTGRES_HOST,
+//   POSTGRES_DB,
+//   POSTGRES_PASSWORD,
+//   POSTGRES_USER,
+
+//   ENV
+// } = process.env
+
+// if (ENV === 'dev') {
+//   client = new Pool({
+//     host: POSTGRES_HOST,
+//     database: POSTGRES_DB,
+//     user: POSTGRES_USER,
+//     password: POSTGRES_PASSWORD,
+//   })
+// }
+
+// if (ENV === 'test') {
+//   client = new Pool({
+//     host: POSTGRES_HOST,
+//     database: POSTGRES_DB,
+//     user: POSTGRES_USER,
+//     password: POSTGRES_PASSWORD
+//   })
+// }
+// export default client
